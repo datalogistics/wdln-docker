2e1be6cb7fa74dd3d29c8267da5c600136cc1cf0
Ezra Kissel <ezkissel@indiana.edu>
2018-10-01 15:55:09 -0400

    fix libdlt git branch

--- wdln_setup.sh
+++ wdln_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build --no-cache -t "wdln-base" base/
+docker build -t "wdln-base" base/
 docker build -t "wdln-ferry" ferry/
 docker build -t "wdln-idms" idms/
 docker build -t "wdln-uploader" uploader/
