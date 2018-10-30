4bd0ae02b827530f06a6c87f721c4198b863df19
Ezra Kissel <ezkissel@indiana.edu>
2018-10-30 10:53:49 -0400

    Updates to cached schema files and uploader setup.

--- idms/run.sh
+++ idms/run.sh
@@ -4,5 +4,7 @@ git -C libdlt pull
 git -C wildfire-dln pull
 
 echo "idms IP : `hostname --ip-address`"
+#idms -u http://wdln-base:9000 -H wdln-idms -p 9001 -d $DEBUG \
+#     -D .dlt -v http://wdln-base -q 42424
 idms -u http://wdln-base:9000 -H wdln-idms -p 9001 -d $DEBUG \
-     -D .dlt -v http://wdln-base -q 42424
+     -D .dlt
